4ddd8a76d8494e81572ab11bed7b73bfd7f7b1ed
Noritsuna Imamura <noritsuna@siprop.org>
2023-09-06 21:43:44 +0900

    1st commit.
    1st commit.

--- src/test.py
+++ src/test.py
@@ -3,46 +3,45 @@ from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
-segments = [ 63, 6, 91, 79, 102, 109, 124, 7, 127, 103 ]
-
 @cocotb.test()
-async def test_7seg(dut):
+async def test_8bitcounter_AI(dut):
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     # reset
-    dut._log.info("reset")
+    dut._log.info("check reset")
     dut.rst_n.value = 0
-    # set the compare value
-    dut.ui_in.value = 1
+    await ClockCycles(dut.clk, 1)
     await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+    assert dut.uo_out.value == 0
 
-    # the compare value is shifted 10 bits inside the design to allow slower counting
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
-
-        # all bidirectionals are set to output
-        assert dut.uio_oe == 0xFF
-
-    # reset
+    dut._log.info("check ena disable." + str(dut.uo_out.value))
+    dut.ena.value = 0
     dut.rst_n.value = 0
-    # set a different compare value
-    dut.ui_in.value = 3
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 10)
+    assert dut.uo_out.value == 0
 
-    max_count = dut.ui_in.value << 10
-    dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
-    # check all segments and roll over
-    for i in range(15):
-        dut._log.info("check segment {}".format(i))
-        await ClockCycles(dut.clk, max_count)
-        assert int(dut.segments.value) == segments[i % 10]
+    dut._log.info("check ena enable.")
+    dut.ena.value = 1
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+
+    await ClockCycles(dut.clk, 20)
+    dut._log.info("check uo_out=" + str(dut.uo_out.value))
+    assert dut.uo_out.value == 20
+    await ClockCycles(dut.clk, 10)
+    dut._log.info("check uo_out=" + str(dut.uo_out.value))
+    assert dut.uo_out.value == 30
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 1)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+    for i in range(255):
+        await ClockCycles(dut.clk, 1)
+        dut._log.info("check uo_out=" + str(dut.uo_out.value) + ", i=" + str(i))
+        assert dut.uo_out.value == i+1
 
