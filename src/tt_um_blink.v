927767bcc55182ebebe7557c66e2aa70852c650f
Junichi Akita <akita@ifdl.jp>
2023-09-10 10:17:36 +0800

    add blink

--- src/tt_um_blink.v
+++ src/tt_um_blink.v
@@ -5,25 +5,19 @@ Prompt:
 Please generate a 8 bits counter in verilog with the following conditions. The argument to be used are input wire [7:0] ui_in, output wire [7:0] uo_out, input  wire [7:0] uio_in, output wire [7:0] uio_out, output wire [7:0] uio_oe, input wire ena, input wire clk, input wire rst_n. The counter variable shall be reg [7:0] count. reg [7:0] count is assign to output wire [7:0] uo_out. The input should only be count. The output should only be count. Count up the count variable by increasing the count variable by 1. The module name is tt_um_noritsuna_8bitcounter_AI.
 */
 `default_nettype none
-module tt_um_noritsuna_8bitcounter_AI(
-        input wire [7:0] ui_in,
-        output wire [7:0] uo_out,
-        input  wire [7:0] uio_in,
-        output wire [7:0] uio_out,
-        output wire [7:0] uio_oe,
-        input wire ena,
-        input wire clk,
-        input wire rst_n);
+module tt_um_blink(en, q);
+   input rst;
+   output [7:0] q;
+   reg [7:0] cnt;
+   wire ck;
+   
+   always @(posedge ck) begin
+     if (rst == 1'b1) cnt <= 0;
+     else cnt <= cnt + 1;
+   end
 
-reg [7:0] count;
-assign uo_out = count[7:0];
-assign uio_out = count[7:0];
-assign uio_oe = ena == 1'b1 && count < 8'b11111111;
-always @(posedge clk) begin
-        if (rst_n == 1'b0) begin
-                count <= 'b0;
-        end else if (ena == 1'b1) begin
-                count <= count + 1'b1;
-        end
-end
+   wire n[20:0];
+   assign n[20:1] = ~n[19:0];
+   assign n[0] = ~n[20];
+		    
 endmodule
