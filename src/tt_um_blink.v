a276ef75bbeb396171621448ac628c299391dd1b
Junichi Akita <akita@ifdl.jp>
2023-09-16 06:28:28 +0800

    Update tt_um_blink.v

--- src/tt_um_blink.v
+++ src/tt_um_blink.v
@@ -1,6 +1,6 @@
 `default_nettype none
-module tt_um_blink(
-        input wire       ui_in,
+module tt_um_noritsuna_8bitcounter_AI(
+        input wire [7:0] ui_in,
         output wire [7:0] uo_out,
         input  wire [7:0] uio_in,
         output wire [7:0] uio_out,
@@ -9,29 +9,16 @@ module tt_um_blink(
         input wire clk,
         input wire rst_n);
 
-   reg [7:0] 	   cnt;
-   wire 	   ck;
+reg [7:0] count;
+assign uo_out = count[7:0];
+assign uio_out = count[7:0];
+assign uio_oe = ena == 1'b1 && count < 8'b11111111;
+always @(posedge clk) begin
+        if (rst_n == 1'b0) begin
+                count <= 'b0;
+        end else if (ena == 1'b1) begin
+                count <= count + 1'b1;
+        end
+end
+endmodule // tt_um_noritsuna_8bitcounter_AI
 
-   wire N1, N2, N3;
-
-   assign N1 = ui_in;
-   assign uo_out = {cnt[7:1], N3};
-   assign uio_oe = ena == 1'b1 && cnt < 8'b11111111;
-
-   always @(posedge ck) begin
-      if (rst_n == 1'b0) cnt <= 0;
-      else cnt <= cnt + 1;
-   end
-
-   not (N2, N1);
-   not (N3, N2);
-//   not (N3, N1);
-   buf (ck, N3);
-//   assign ck = N3;
-
-//   wire [20:0] n;
-//   assign n[20:1] = ~n[19:0];
-//   assign n[0] = ~n[20];
-//   assign ck = ~n[0];
-
-endmodule // tt_um_blink
